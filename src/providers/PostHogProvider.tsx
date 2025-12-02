e9878fe74028be04c6366ebe0c32617784db2f9a
Renato <wizardfolioco@gmail.com>
2025-12-02 13:07:45 -0500

    Revert "1"
    This reverts commit 216fa8eaef7b3660ce776f9b30e73283683e6543.

--- src/providers/PostHogProvider.tsx
+++ src/providers/PostHogProvider.tsx
@@ -15,7 +15,7 @@ export function PostHogProvider({ children }: PostHogProviderProps) {
     if (!posthog.__loaded) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
         api_host:
-          process.env.NEXTPUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
         capture_pageview: false,
         autocapture: true,
       });
